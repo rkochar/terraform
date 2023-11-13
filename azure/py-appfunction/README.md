0f56f88a17d221b9add2fc3ae61f3ecb645ca7cb
rkochar <rkochar9@gmail.com>
2023-11-13 11:04:49 +0100

    first commit

---
+++ azure/py-appfunction/README.md
@@ -0,0 +1,6 @@
+A simple hello-world faas in Azure.
+
+# 1
+`tf apply` in terraform and `func azure functionapp publish tfappfunction-dev-function-app` in the function dir.
+
+# 2
---
+++ azure/py-appfunction/terraform/variables.tf
@@ -0,0 +1,14 @@
+variable "project" {
+  type = string
+  default = "my-project"
+}
+
+variable "environment" {
+  type = string
+  default = "dev"
+}
+
+variable "location" {
+  type = string
+  default = "West Europe"
+}
